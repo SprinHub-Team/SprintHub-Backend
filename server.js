b394d351be8980b4a939aab9ad302696a94766c6
Eduar Zorro <Zorroedward0@gmail.com>
2026-08-05 11:16:29 -0500

    fix: server.js duplicate lines.

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
-const dontenv = require('dotenv');
-dontenv.config();
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
 const port = process.env.PORT;
 
@@ -8,8 +8,15 @@ const port = process.env.PORT;
 app.use(express.json());
 
 // Sample route
-app.get('/hello', (req, res) => {
+app.get('/', (req, res) => {
   res.send('Hello, World!');
+});
+
+app.get('/data', (req, res) => {
+  const sampleData = {
+    message: 'This is some sample data from the server.',
+  }
+  res.json(sampleData);
 });
 
 // Start the server
