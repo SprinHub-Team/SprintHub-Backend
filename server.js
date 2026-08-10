a2240d0cb45e2bd1e6c5fb2d4620677f93f7bced
Eduar Zorro <Zorroedward0@gmail.com>
2026-08-10 09:09:14 -0500

    feat: creando definicion basica de las rutas

--- server.js
+++ server.js
@@ -1,25 +1,15 @@
-const express = require('express');
-const dontenv = require('dotenv');
-dontenv.config();
-const app = express();
-const port = process.env.PORT;
+import express from "express";
+import dotenv from "dotenv";
+import helloRoutes from "./routes/helloRoutes.js";
 
+dotenv.config();
+
+const app = express();
+const port = process.env.PORT || 3000;
 
 app.use(express.json());
 
-app.get('/hello', (req, res) => {
-  simpleObject = {
-    name: 'John Doe',
-    age: 30,
-    email: 'john.doe@example.com'
-  };
-  res.json(simpleObject);
-});
-
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
+app.use('/hello', helloRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
