bae29e1bd353c2850d0cdfb5174af27389ce1cb9
Zorro <zorroedward0@gmail.com>
2026-08-26 22:29:49 -0500

    fix: correcion de repositorys para implementacion coorecta de tipos, inclusion dezod para avlidaciones de tipo a nivel de ejecucion (Board, Column)

---
+++ src/dtos/ColumnDto.ts
@@ -0,0 +1,20 @@
+import {z} from 'zod';
+
+export const columnSchema = z.object({
+  id: z.string(),
+  name: z.string().min(2).max(150),
+  boardId: z.string(),
+  cardsId: z.array(z.string())
+});
+
+export type Column = z.infer< typeof columnSchema>;
+
+export const createColumnSchema = columnSchema.omit({
+  id: true
+});
+
+export type CreateColumnDto = z.infer<typeof createColumnSchema>;
+
+export const updateColumnSchema = createColumnSchema.partial();
+
+export type UpdateColumnDto = z.infer<typeof updateColumnSchema>;
